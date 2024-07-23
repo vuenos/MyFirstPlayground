1288531380ba36088b121382580b64dd6a9c0ec3
jintae kim <vuenos@naver.com>
2024-07-23 23:04:11 +0900

    변수의 정의

--- MyFirstPlayground.playground/Pages/Variable.xcplaygroundpage/Contents.swift
+++ MyFirstPlayground.playground/Pages/Variable.xcplaygroundpage/Contents.swift
@@ -1,7 +1,19 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var greeting = "Hello, playground"
+// 상수 : 변경되지 않는 값 (let)
+// 변수 : 변경되는 값을 저장 (var)
+// 상수 >> 변수
+
+var name = "Jintae kim"
+
+name = "Jiwoo kim"
+
+print(name)
+
+
+// Type Annotation
+// Type Safety
 
 //: [Next](@next)
